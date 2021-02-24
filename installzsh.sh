e4209e4c3be816961459b6d143d3e7454b7933ee
cj <charles.jobin@gmail.com>
2021-02-24 14:12:53 -0500

    added the change default shell command and running zsh at the end

--- installzsh.sh
+++ installzsh.sh
@@ -20,6 +20,10 @@ function install() {
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     sed -i 's/plugins=(git)/plugins=( git zsh-syntax-highlighting zsh-autosuggestions )/' ~/.zshrc
+
+    chsh -s $(which zsh)
+    zsh
+
 }
 
 ### Independent steps depending on platform
