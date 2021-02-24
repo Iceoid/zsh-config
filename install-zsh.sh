cc7101d95ed3b673f1a31d49008682dc5b88a5a7
cj <charles.jobin@gmail.com>
2021-02-23 21:07:46 -0500

    added install script

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+SUDO=""
+
+if [[ $EUID -ne 0 ]]; then
+   SUDO='sudo'
+fi
+
+function install_oh-my-zsh() {
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="fino"/' ~/.zshconfig
+    echo 'alias ll="ls -hal"' >> ~/.zshconfig
+}
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     
+        machine=Linux
+        ${SUDO} apt update
+        ${SUDO} apt install curl zsh -y
+        install_oh-my-zsh
+        ;;
+    Darwin*)
+        machine=Mac
+        install_oh-my-zsh
+        ;;
+    *)
+        machine="OTHER:${unameOut}"
+        ;;
+esac
+echo ${machine}
