6d6a43d842704b7dbcb999de1c1bc1dc178ab202
cj <charles.jobin@gmail.com>
2021-03-05 18:37:56 +0000

    moved the prompt option addition at the end of the install script.

--- installzsh.sh
+++ installzsh.sh
@@ -50,14 +50,13 @@ function install() {
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
     echo 'alias ll="ls -hal"' >> ~/.zshrc
-    echo "typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=same-dir" >> ~/.p10k.zsh
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     sed -i 's/plugins=(git)/plugins=( git zsh-syntax-highlighting zsh-autosuggestions )/' ~/.zshrc
 
     chsh -s $(which zsh)
     zsh
-
+    echo "typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=same-dir" >> ~/.p10k.zsh
 }
 
 ### Independent steps depending on platform
