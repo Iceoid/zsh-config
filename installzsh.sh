9211a02cfaedf707f76121b9e10b050574c8f0e2
Charles Fauvel <cf@mbpcj-eth.local>
2021-11-14 20:35:35 -0500

    macOS: installing p10k and fonts through brew.

--- installzsh.sh
+++ installzsh.sh
@@ -48,15 +48,23 @@ function install() {
     cd .. && rm -R .zshinstall
 
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+    sed -i '' 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
     echo 'alias ll="ls -hal"' >> ~/.zshrc
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    sed -i 's/plugins=(git)/plugins=( git zsh-syntax-highlighting zsh-autosuggestions )/' ~/.zshrc
+    sed -i '' 's/plugins=(git)/plugins=( git zsh-syntax-highlighting zsh-autosuggestions )/' ~/.zshrc
 
     chsh -s $(which zsh)
     zsh
     echo "typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=same-dir" >> ~/.p10k.zsh
+}
+
+function install_fonts() {
+    cd $1 && curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf && \
+        curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf &&\
+        curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf && \
+        curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+    
 }
 
 ### Independent steps depending on platform
@@ -69,12 +77,13 @@ case "${unameOut}" in
         ${SUDO} $(update_packages)
         ${SUDO} $(install_packages) curl wget git zsh -y
 
-        mkdir -p ~/.local/share/fonts && \
-        cd ~/.local/share/fonts && \
-        curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf && \
-        curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf &&\
-        curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf && \
-        curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+        FONT_DIR=~/.local/share/fonts
+        mkdir -p $FONT_DIR && cd $FONT_DIR && install_fonts $FONT_DIR
+        # cd ~/.local/share/fonts && \
+        # curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf && \
+        # curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf &&\
+        # curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf && \
+        # curl -L -O https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
         install
 
@@ -84,7 +93,25 @@ case "${unameOut}" in
         machine=Mac
         echo "Installing on ${unameOut}"
 
-        install
+        which brewa
+        if [[ "$?" == 0 ]] ; then
+            echo 'brew found'
+        else
+            echo "brew not found"
+            echo "Installing brew..."
+            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        fi
+        
+        brew tap homebrew/cask-fonts
+        brew install --cask font-meslo-lg-nerd-font
+
+        brew install romkatv/powerlevel10k/powerlevel10k
+        echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+
+        osascript -e "tell application \"Terminal\" to set the font name of window 1 to \"MesloLGM Nerd Font\""
+        osascript -e "tell application \"Terminal\" to set the font size of window 1 to 12"
+
+        # install
         ;;
     *)
         machine="OTHER:${unameOut}"
